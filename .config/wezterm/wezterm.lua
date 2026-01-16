63bc17b827b97a6d569ef4d0eb953e6ccdd09b33
marcwoozie <marcwoozie.github@gmail.com>
2026-01-16 18:06:25 +0900

    update wezterm blur

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ config.automatically_reload_config = true
 config.font_size = 11.0
 config.use_ime = true
 config.window_background_opacity = 0.8
-config.macos_window_background_blur = 2
+config.macos_window_background_blur = 0
 config.color_scheme = "tokyonight"
 
 ----------------------------------------------------
@@ -22,13 +22,13 @@ config.hide_tab_bar_if_only_one_tab = true
 
 -- タブバーの透過
 config.window_frame = {
-  inactive_titlebar_bg = "none",
-  active_titlebar_bg = "none",
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
 }
 
 -- タブバーを背景色に合わせる
 config.window_background_gradient = {
-  colors = { "#000000" },
+	colors = { "#000000" },
 }
 
 -- タブの追加ボタンを非表示
@@ -39,9 +39,9 @@ config.show_close_tab_button_in_tabs = false
 
 -- タブ同士の境界線を非表示
 config.colors = {
-  tab_bar = {
-    inactive_tab_edge = "none",
-  },
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
 }
 
 -- タブの形をカスタマイズ
@@ -51,26 +51,26 @@ local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = "#5c6d74"
-  local foreground = "#FFFFFF"
-  local edge_background = "none"
-  if tab.is_active then
-    background = "#59c2c6"
-    foreground = "#091833"
-  end
-  local edge_foreground = background
-  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
-  return {
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_LEFT_ARROW },
-    { Background = { Color = background } },
-    { Foreground = { Color = foreground } },
-    { Text = title },
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_RIGHT_ARROW },
-  }
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+	local edge_background = "none"
+	if tab.is_active then
+		background = "#59c2c6"
+		foreground = "#091833"
+	end
+	local edge_foreground = background
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
 end)
 
 -- タブなどを消す際のコンファーム画面をパスする設定_すべての確認コンファームを出す
