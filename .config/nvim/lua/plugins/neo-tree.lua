ecd24e4c9a3863cd7e217758d76977cf366a071f
marcwoozie <marcwoozie.github@gmail.com>
2026-03-30 01:03:09 +0900

    setup neo-tree plugin

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,15 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle<CR>", mode = "n" }
+    },
     opts = {
       filesystem = {
         filtered_items = {
