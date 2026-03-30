467d76e6b008e3462660d563eff8483105953958
marcwoozie <marcwoozie.github@gmail.com>
2026-03-31 00:32:48 +0900

    add: lualine plugin

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,1 +1,9 @@
--- Options are loaded before lazy.nvim startup.
+vim.g.autoformat = true
+
+local opt = vim.opt
+
+opt.relativenumber = true -- Relative line numbers
+opt.number = true -- Absolute line numbers
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.mouse = "a" -- Enable mouse mode
+
